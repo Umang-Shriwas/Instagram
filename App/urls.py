512b741013fb9b4cc9360c878ca3cdc016182ac1
Umang-Shriwas <umangshriwas17@gmail.com>
2022-07-07 12:32:56 +0530

    commit

--- App/urls.py
+++ App/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path('',views.index,name='index'),
     path('signup/',views.create_profile,name="signup"),
     path('login/',views.Login,name="Login"),
+    path('logout/',views.Logout,name="logout"),
     path('profile/',views.profile,name="pr"),
+    path('search/',views.search,name="search"),
 
 ]
--- App/views.py
+++ App/views.py
@@ -2,14 +2,22 @@ from cProfile import Profile
 from email import message
 from django.shortcuts import redirect, render
 from .models import Profile
-from django.core.checks import messages
+from django.contrib import messages
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
 
 # Create your views here.
 def index(request):
     return render(request,'index.html')
 
 def Login(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        user=authenticate(username=username,password=password)
+        if user:
+            login(request,user)
+            return redirect('pr')
     return render(request,'Login.html')
 
 def create_profile(request):
@@ -25,6 +33,15 @@ def create_profile(request):
     return render(request,'SignUp.html')
 
 def profile(request):
-    
-    return render(request,'profile.html')
+    profile=Profile.objects.get(user=request.user)
+    return render(request,'profile.html',{'profile':profile,'profile_user':True})
 
+def Logout(request):
+    logout(request)
+    return redirect("Login")
+
+def search(request):
+    search=request.GET['username']
+    profiles=Profile.objects.filter(user__username__icontains=search)
+    context={'profiles':profiles}
+    return render(request,'search.html',context)
